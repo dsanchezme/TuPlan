8dfeb1656150a75377d1c5d15d7b58bd5b240655
cdsaineac <cdsaineac@unal.edu.co>
2022-10-20 00:40:24 -0500

    Form fixed

--- app/src/main/java/com/dadm/tuplan/CreateTaskActivity.java
+++ app/src/main/java/com/dadm/tuplan/CreateTaskActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,16 +20,28 @@ import com.dadm.tuplan.models.User;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
-public class CreateTaskActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+public class CreateTaskActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private TextView inputTitle;
     private TextView inputDescription;
-    private TextView inputStartDate;
-    private TextView inputPriority;
-    private TextView inputStatus;
+    private DatePicker inputStartDate;
+    private Spinner inputPriority;
+    private Spinner inputStatus;
     private Button buttonCreatePlan;
     private Snackbar mySnackbar;
     private PlanDAO planDAO;
     private User currentUser;
+
+    private String[] arrayStatus = {"Completada","Pendiente"};
+    private String[] arrayPriority = {"Baja","Media","Alta"};
+
+    private ArrayAdapter<String> aaStatus;
+    private ArrayAdapter<String> aaPriority;
+    private String prioritySelected;
+    private String statusSelected;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,23 +86,42 @@ public class CreateTaskActivity extends AppCompatActivity {
         });
     }
     private void initViews(){
+        aaStatus = new ArrayAdapter<>(this,R.layout.layout_list_item,arrayStatus);
+        aaPriority = new ArrayAdapter<>(this,R.layout.layout_list_item,arrayPriority);
+        inputStatus =(Spinner) findViewById(R.id.status_input);
+        inputPriority =(Spinner) findViewById(R.id.priority_input);
+
+        inputStatus.setAdapter(aaStatus);
+        inputPriority.setAdapter(aaPriority);
+
+        inputStatus.setOnItemSelectedListener(this);
+        inputPriority.setOnItemSelectedListener(this);
+
+
         inputTitle =(TextView) findViewById(R.id.title_input);
         inputDescription =(TextView) findViewById(R.id.description_input);
-        inputStatus =(TextView) findViewById(R.id.status_input);
-        inputStartDate =(TextView) findViewById(R.id.start_date_input);
-        inputPriority =(TextView) findViewById(R.id.priority_input);
+        inputStartDate =(DatePicker) findViewById(R.id.start_date_input);
         buttonCreatePlan = (Button) findViewById(R.id.buttonCreatePlan);
         buttonCreatePlan.setOnClickListener(view -> {
             handleClickCreation();
         });
     }
     private void handleClickCreation(){
+        int year = inputStartDate.getYear();
+        int month = inputStartDate.getMonth();
+        int day = inputStartDate.getDayOfMonth();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+
+        SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+        String startDate = format.format(calendar.getTime());
+
 
         String title =  inputTitle.getText().toString();
         String desc = inputDescription.getText().toString();
-        String status = inputStatus.getText().toString();
-        String startDate =  inputStartDate.getText().toString();
-        String priority = inputPriority.getText().toString();
+        String status = statusSelected;
+        String priority = prioritySelected;
         Plan newPlan = new Plan(title,desc,status,startDate,currentUser,priority);
         long id = planDAO.getCountChildren();
         planDAO.addPlan(String.valueOf(id+1),newPlan);
@@ -95,5 +130,23 @@ public class CreateTaskActivity extends AppCompatActivity {
         Intent toHome = new Intent(CreateTaskActivity.this, HomeActivity.class);
         toHome.putExtra("user",(User) getIntent().getExtras().get("user"));
         startActivity(toHome);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Spinner spinner = (Spinner) adapterView;
+        if(spinner.getId() == R.id.status_input)
+        {
+           statusSelected = spinner.getSelectedItem().toString();
+        }
+        else if(spinner.getId() == R.id.priority_input)
+        {
+            prioritySelected = spinner.getSelectedItem().toString();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
